3f1eddf811bc5050cf31ece9bec980a5da57300a
trickyartem <50799247+trickyartem@users.noreply.github.com>
2019-08-02 08:11:49 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const canvas = document.createElement('canvas');
+const c = canvas.getContext('2d');
+canvas.height = innerHeight
+canvas.width = innerWidth
+
+canvas.style.position = "fixed";
+canvas.style.top = "0px";
+canvas.style.left = "0px";
+canvas.style.background = "lightblue";
+
+document.body.appendChild(canvas);
+console.log("HELP ME PLS");
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  watch: true,
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index.js',
+  },
+  devServer: {
+    inline: true,
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      Src: path.resolve(__dirname, 'src')
+    },
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.json', '.scss', '.sass']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
